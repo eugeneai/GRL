cd0d958e2b6d7f573c19052a1856af9f1ae66fe0
de17eon <de17eon@gmail.com>
2021-05-20 19:11:21 +0800

    Подключил файл со всеми данными

--- src/store/reducers/async/loadAllData.js
+++ src/store/reducers/async/loadAllData.js
@@ -3,7 +3,7 @@ import { load_all_data } from "../dataReducer";
 export const fetchFeatures = () => {
   return (dispatch) => {
 
-    fetch("geojson.json")
+    fetch("data.json")
       .then(response => response.json())
       .then(data => {
         dispatch(load_all_data(data.features)) 
