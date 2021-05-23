00ecf06413f21b3b756324d07992cfe857862618
de17eon <de17eon@gmail.com>
2021-05-24 01:21:45 +0800

    Big Commit: Добавил setTimeout благодаря которому появляется динамика

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react'
 import { MapContainer, Marker, Popup } from 'react-leaflet'
-import { useSelector } from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import './map.css'
 
 import ListBaseMaps from './ListBaseMaps'
@@ -8,12 +8,15 @@ import GeoJsonLayer from './GeoJsonLayer'
 
 export default function LeafletMap() {
   
+  const dispatch = useDispatch()
   const position = [53.105, 107.405]
   const visible = useSelector(state => state.interface.commonVisible)
   const lineParameters = useSelector(state => state.interface.lineParameters)
 
   useEffect(() => {
     console.log("LeafletMap", {...lineParameters})
+    dispatch({type: "TOOGLE_VISIBLE"})
+    setTimeout(() => dispatch({type: "TOOGLE_VISIBLE"}), 25)
   }, [lineParameters])
 
   return (
