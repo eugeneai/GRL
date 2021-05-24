9da47a7ed86a397a844f2c791e27406998fe2d3d
de17eon <de17eon@gmail.com>
2021-05-24 19:41:32 +0800

    Добавил обрабочики в редюсер

--- src/store/reducers/sparqlReducer.js
+++ src/store/reducers/sparqlReducer.js
@@ -4,13 +4,23 @@ const initState = {
 
 const sparqlReducer = (state = initState, action) => {
   switch (action.type) {
-    case "Load":
+    case "LOAD":
       return {...state, test: action.payload}
+    
+    // Пересечение множеств
+    case "INTERSECTION": {
+      return {...state, test: this.state.test.filter(id => action.payload.includes(id))}
+    }
+    
+    case "CLEAR": {
+      return {...state, test: []}
+    }
+    
     default:
       return state
   }
 }
 
-export const tLoad = (payload) => ({type: "Load", payload})
+export const tLoad = (payload) => ({type: "LOAD", payload})
 
 export default sparqlReducer
