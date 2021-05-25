9920cf43986baefd7d5defaea8d0bb878f9b5578
de17eon <de17eon@gmail.com>
2021-05-25 17:02:25 +0800

    Обновил редюсер

--- src/store/reducers/sparqlReducer.js
+++ src/store/reducers/sparqlReducer.js
@@ -1,19 +1,52 @@
 const initState = {
-  test: []
+  displayedData: [],
+  sortedData: {
+    dataBySeismic: [],
+    dataByHistoricAge: [],
+    dataByKinematicType: []
+  }
+  
 }
 
 const sparqlReducer = (state = initState, action) => {
   switch (action.type) {
-    case "LOAD":
-      return {...state, test: action.payload}
+    
+    case "LOAD_DATA_BY_SEISMIC":
+      return {...state, sortedData: { ...this.state.sortedData, dataBySeismic: action.payload}}
+
+    case "LOAD_DATA_BY_HISTORIC_AGE":
+      return {...state, sortedData: { ...this.state.sortedData, dataByHistoricAge: action.payload}}
+
+    case "LOAD_DATA_BY_KINEMATIC_TYPE":
+      return {...state, sortedData: { ...this.state.sortedData, dataByKinematicType: action.payload}}
     
     // Пересечение множеств
     case "INTERSECTION": {
-      return {...state, test: this.state.test.filter(id => action.payload.includes(id))}
+      
+      let filters = []
+      for (let dataByOption in state.sortedData) {
+        if (dataByOption.length > 0) {
+          filters.push(dataByOption)
+        }
+      }
+      
+      if (filters.length === 0) {
+        return {...state, displayedData: []}
+      }
+      if (filters.length === 1) {
+        return {...state, displayedData: filters[0]}
+      }
+
+      let result = filters.shift()
+      for (const f in filters) {
+        result.filter(id => f.includes(id))
+      }
+      
+      return {...state, displayedData: result}
     }
     
     case "CLEAR": {
-      return {...state, test: []}
+      return {...state, displayedData: []}
     }
     
     default:
