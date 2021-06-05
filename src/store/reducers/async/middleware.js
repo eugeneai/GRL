99af6a15ff067496e322b3fef3527ddc6f84d816
de17eon <de17eon@gmail.com>
2021-06-05 14:50:09 +0800

    Добавил функцию получения информации о разломе

--- src/store/reducers/async/middleware.js
+++ src/store/reducers/async/middleware.js
@@ -90,3 +90,41 @@ export const fetchIdsBy = (tabId, ops, action) => {
       })
   }
 }
+
+export const fetchInfoByID = (id) => {
+  
+  return (dispatch) => {
+    const query = `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX : <http://192.168.2.2:7200/repositories/ActiveFaults#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    SELECT ?label ?o
+    WHERE { 
+        :${id} ?p ?o.
+        ?p rdfs:label ?label FILTER (langMatches(lang(?label), "ru")).
+    }`.replace(/ +/g, ' ').trim()
+
+    const encodedQuery = "http://localhost:7200/repositories/ActiveFaults?query=" + encodeURIComponent(query)
+
+    fetch(encodedQuery)
+      .then(response => response.text())
+      .then(data => {
+        console.log("data: ", data)
+        const array = data.replace(/\r/g, "").split("\n")
+        for (let num in array) {
+          const border = array[num].indexOf(",")
+          const nameProp = array[num].slice(0, border)
+          const valueProp = array[num].slice(border+1)
+          array[num] = [nameProp, valueProp]
+        }
+        array.shift()
+        array.pop()
+        console.log("array: ", array)
+        return array
+      })
+      .then(
+        data => dispatch({type: "SET_INFO_BY_ID", payload: data}),
+        error => console.error(error)
+      )
+
+  }
+}
